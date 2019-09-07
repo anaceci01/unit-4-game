1c83981396c49808d0da57f8ae8b209c1ded4373
Ana Davis <anac.davis@gmail.com>
2019-09-06 19:18:48 -0700

    added 4 crystal boxes and udated on how to generate random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,34 @@
 
-var result= 0;
-var win= 0;
-var lost= 0;
-var target= 0;
-var max = 120;
-var min = 19;
+var result;
+var win;
+var lost;
+var target;
+var max;
+var min;
 
 
-var randomNumber = Math.floor(Math.random() * (max - min + 1))+ min;
-console.log(randomNumber);
+for(var i = 0; i < 4; i++) {
 
+    var random = Math.floor(Math.random() * 12);
+    console.log(random);
+
+    var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": random
+        });
+
+    $(".crystals").append(crystal);
+}
+        
 // Computer generates a random number between 19 and 120
 // The random number will display on the Target section
+
+
+
+
+
+
 // Each one of the 4 crystals will be assigned computer generated numbers, values between 1-19
 // The value give is going to be reset each time the game starts
 
