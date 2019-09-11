9328f801cab1385c0398cc21f2a7cfd4d1f58d25
Ana Davis <anac.davis@gmail.com>
2019-09-10 23:51:37 -0700

    resetGame funciton working as well as winCount stop adding every click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,63 +2,65 @@
 var result = 0;
 var win = 0;
 var lost = 0;
-var target;
+var target=0;
 var max = 120;
 var min = 19;
 
 // Computer generates a random number between 19 and 120
 // The random number will display on the Target section
+// Each one of the 4 crystals will be assigned computer generated numbers, values between 1-19
 
-target = Math.floor(Math.random() * (max - min + 1)) + min;
+    var resetGame = function(){
+        result = 0;
+        target = Math.floor(Math.random() * (max - min + 1));
+        $("#target").html("Target Number: " + target);
+    }
 
+        var images = [
+            "assets/images/crystal-1.jpg", 
+            "assets/images/crystal-2.jpg",
+            "assets/images/crystal-3.jpg",
+            "assets/images/crystal-4.jpg"
+        ];
+
+target = Math.floor(Math.random() * (max - min + 1));
 $("#target").html("Target Number: " + target);
 
-// Each one of the 4 crystals will be assigned computer generated numbers, values between 1-19
 for (var i = 0; i < 4; i++) {
     var random = Math.floor(Math.random() * 18) + 1;
     console.log(random);
 
     var crystal = $("<div>");
     crystal.attr({
-        "class": 'crystal',
-        "data-random": random
+        "class": "crystal",
+        "data-random": random,
+        "background-image": images;
     });
 
+    
     $(".crystals").append(crystal);
-}
+    }
+//}
 
-$(".crystal").on("click", function () {
+//resetGame ();
+
+$(document).on("click", ".crystal", function () {
     var value = ($(this).attr("data-random"));
     value = parseInt(value);
     result += value;
 
-
-
     if (result === target) {
         wins++;
+        resetGame();
     }
 
     else if (result >= target) {
         lost++;
+        resetGame();
     }
+
     $("#score").html(result);
     $("#loss").html(lost);
     $("#wins").html(win);
-
-
-
-
-
-
-
 });
-
-
-
-
-
-
-
-// The value give is going to be reset each time the game starts
-
 
