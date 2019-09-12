c80cbdce8f918d7e31cb79f9308ab84ce18d336c
Ana Davis <anac.davis@gmail.com>
2019-09-12 16:36:40 -0700

    crystal images added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var result = 0;
 var win = 0;
 var lost = 0;
-var target=0;
+var target = 0;
 var max = 120;
 var min = 19;
 
@@ -10,18 +10,19 @@ var min = 19;
 // The random number will display on the Target section
 // Each one of the 4 crystals will be assigned computer generated numbers, values between 1-19
 
-    var resetGame = function(){
-        result = 0;
-        target = Math.floor(Math.random() * (max - min + 1));
-        $("#target").html("Target Number: " + target);
-    }
+var resetGame = function () {
+    result = 0;
+    target = Math.floor(Math.random() * (max - min + 1));
+    $("#target").html("Target Number: " + target);
+}
 
-        var images = [
-            "assets/images/crystal-1.jpg", 
-            "assets/images/crystal-2.jpg",
-            "assets/images/crystal-3.jpg",
-            "assets/images/crystal-4.jpg"
-        ];
+var imageArray = [
+    "http://33.media.tumblr.com/tumblr_m0lgiu6OYP1qh3v5mo1_500.jpg",
+    "http://40.media.tumblr.com/90e88e0ade888ac9c5e532e8fbf1d0b9/tumblr_ncddxs3FZG1sqjjogo1_500.jpg",
+    "https://thumbs.dreamstime.com/b/abstract-rainbow-holographic-foil-texture-trendy-magic-background-pastel-colours-131503756.jpg",
+    "https://data.whicdn.com/images/316261532/large.jpg"
+];
+var imageIndex = 0;
 
 target = Math.floor(Math.random() * (max - min + 1));
 $("#target").html("Target Number: " + target);
@@ -34,15 +35,16 @@ for (var i = 0; i < 4; i++) {
     crystal.attr({
         "class": "crystal",
         "data-random": random,
-        "background-image": images;
     });
 
-    
-    $(".crystals").append(crystal);
-    }
-//}
+    crystal.css({
+        "background-image":"url('" + imageArray[i] + "')"
+    });
 
-//resetGame ();
+    $(".crystals").append(crystal);
+}
+
+
 
 $(document).on("click", ".crystal", function () {
     var value = ($(this).attr("data-random"));
